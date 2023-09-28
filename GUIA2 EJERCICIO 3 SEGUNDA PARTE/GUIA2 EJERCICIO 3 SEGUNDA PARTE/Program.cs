4ee76d2f96dcfabdd0cafb9dca25941f03780fb9
emerson <124007178+Emeson12345678910@users.noreply.github.com>
2023-09-28 14:59:05 -0600

    Add files via upload

---
+++ GUIA2 EJERCICIO 3 SEGUNDA PARTE/GUIA2 EJERCICIO 3 SEGUNDA PARTE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Split
+{
+    public static void Main(string[] args)
+    {
+        string frase = "MI MAMA ME MIMA, AMO A MI MAMA";
+        string[] MAMA= frase.Split(' ');
+
+        foreach (string palabra in MAMA)
+        {
+            Console.WriteLine(palabra);
+        }
+    }
+}
