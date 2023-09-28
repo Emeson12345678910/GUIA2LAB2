a9d67f4cf68af46c9469ba3114c6300f729d0e79
emerson <124007178+Emeson12345678910@users.noreply.github.com>
2023-09-28 14:54:52 -0600

    Add files via upload

---
+++ EJERCICIO GUIA2 LAB2  PARTE 3/EJERCICIO GUIA2 LAB2  PARTE 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string frase = "MI MAMA ME MIMA, AMO A MI MAMA";
+        string[] MAMA = frase.Split(' ');
+
+        for (int i = 0; i < MAMA.Length; i++)
+        {
+            if (MAMA[i].Length < 3)
+            {
+                MAMA[i] += "%";
+            }
+        }
+
+        string resultado = string.Join(" ", MAMA);
+        Console.WriteLine(resultado);
+    }
+}
